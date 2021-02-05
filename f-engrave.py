45540ea7f72ac12bdb56b2e9a7d16ec4d315fc1d
Chris Nisbet <nischris@gmail.com>
2021-02-05 17:41:28 +1300

    Moved WriteDXF into dxf.py

--- f-engrave.py
+++ f-engrave.py
@@ -328,7 +328,7 @@ if PIL == True:
 import binascii
 from constants import Zero, IN_AXIS
 from constants import MIN_METRIC_STEP_LEN, MIN_IMP_STEP_LEN
-from dxf import parse_dxf
+from dxf import parse_dxf, WriteDXF
 import getopt
 from graphics import Character, Line, Get_Angle, Transform
 import font
@@ -2257,175 +2257,6 @@ class Application(Frame):
                 Delta = Thick / 2 + float(self.boxgap.get())
         self.svgcode.append('</svg>')
 
-
-
-    ##################################################
-    ###  Begin Dxf_Write G-Code Writing Function   ###
-    ##################################################
-    def WriteDXF(self, close_loops=False):
-
-        if close_loops:
-            self.V_Carve_It(clean_flag=0, DXF_FLAG = close_loops)
-
-        dxf_code = []
-        # Create a header section just in case the reading software needs it
-        dxf_code.append("999")
-        dxf_code.append("DXF created by G-Code Ripper <by Scorch, www.scorchworks.com>")
-        dxf_code.append("0")
-        dxf_code.append("SECTION")
-        dxf_code.append("2")
-        dxf_code.append("HEADER")
-        #dxf_code.append("9")
-        #dxf_code.append("$INSUNITS")
-        #dxf_code.append("70")
-        #dxf_code.append("1") #units 1 = Inches; 4 = Millimeters;
-        dxf_code.append("0")
-        dxf_code.append("ENDSEC")
-
-        #
-        #Tables Section
-        #These can be used to specify predefined constants, line styles, text styles, view
-        #tables, user coordinate systems, etc. We will only use tables to define some layers
-        #for use later on. Note: not all programs that support DXF import will support
-        #layers and those that do usually insist on the layers being defined before use
-        #
-        # The following will initialise layers 1 and 2 for use with moves and rapid moves.
-        dxf_code.append("0")
-        dxf_code.append("SECTION")
-        dxf_code.append("2")
-        dxf_code.append("TABLES")
-        dxf_code.append("0")
-        dxf_code.append("TABLE")
-        dxf_code.append("2")
-        dxf_code.append("LTYPE")
-        dxf_code.append("70")
-        dxf_code.append("1")
-        dxf_code.append("0")
-        dxf_code.append("LTYPE")
-        dxf_code.append("2")
-        dxf_code.append("CONTINUOUS")
-        dxf_code.append("70")
-        dxf_code.append("64")
-        dxf_code.append("3")
-        dxf_code.append("Solid line")
-        dxf_code.append("72")
-        dxf_code.append("65")
-        dxf_code.append("73")
-        dxf_code.append("0")
-        dxf_code.append("40")
-        dxf_code.append("0.000000")
-        dxf_code.append("0")
-        dxf_code.append("ENDTAB")
-        dxf_code.append("0")
-        dxf_code.append("TABLE")
-        dxf_code.append("2")
-        dxf_code.append("LAYER")
-        dxf_code.append("70")
-        dxf_code.append("6")
-        dxf_code.append("0")
-        dxf_code.append("LAYER")
-        dxf_code.append("2")
-        dxf_code.append("1")
-        dxf_code.append("70")
-        dxf_code.append("64")
-        dxf_code.append("62")
-        dxf_code.append("7")
-        dxf_code.append("6")
-        dxf_code.append("CONTINUOUS")
-        dxf_code.append("0")
-        dxf_code.append("LAYER")
-        dxf_code.append("2")
-        dxf_code.append("2")
-        dxf_code.append("70")
-        dxf_code.append("64")
-        dxf_code.append("62")
-        dxf_code.append("7")
-        dxf_code.append("6")
-        dxf_code.append("CONTINUOUS")
-        dxf_code.append("0")
-        dxf_code.append("ENDTAB")
-        dxf_code.append("0")
-        dxf_code.append("TABLE")
-        dxf_code.append("2")
-        dxf_code.append("STYLE")
-        dxf_code.append("70")
-        dxf_code.append("0")
-        dxf_code.append("0")
-        dxf_code.append("ENDTAB")
-        dxf_code.append("0")
-        dxf_code.append("ENDSEC")
-
-        #This block section is not necessary but apperantly it's good form to include one anyway.
-        #The following is an empty block section.
-        dxf_code.append("0")
-        dxf_code.append("SECTION")
-        dxf_code.append("2")
-        dxf_code.append("BLOCKS")
-        dxf_code.append("0")
-        dxf_code.append("ENDSEC")
-
-        # Start entities section
-        dxf_code.append("0")
-        dxf_code.append("SECTION")
-        dxf_code.append("2")
-        dxf_code.append("ENTITIES")
-        dxf_code.append("  0")
-
-        #################################
-        ## GCODE WRITING for Dxf_Write ##
-        #################################
-        #for line in side:
-        for line in self.coords:
-            XY = line
-
-            #if line[0] == 1 or (line[0] == 0 and Rapids):
-            dxf_code.append("LINE")
-            dxf_code.append("  5")
-            dxf_code.append("30")
-            dxf_code.append("100")
-            dxf_code.append("AcDbEntity")
-            dxf_code.append("  8") #layer Code #dxf_code.append("0")
-
-            ##########################
-            #if line[0] == 1:
-            #    dxf_code.append("1")
-            #else:
-            #    dxf_code.append("2")
-            #dxf_code.append(" 62") #color code
-            #if line[0] == 1:
-            #    dxf_code.append("10")
-            #else:
-            #    dxf_code.append("150")
-            dxf_code.append("1")
-            dxf_code.append(" 62") #color code
-            dxf_code.append("150")
-            ###########################
-
-            dxf_code.append("100")
-            dxf_code.append("AcDbLine")
-            dxf_code.append(" 10")
-            dxf_code.append("%.4f" % (line[0])) #x1 coord
-            dxf_code.append(" 20")
-            dxf_code.append("%.4f" % (line[1])) #y1 coord
-            dxf_code.append(" 30")
-            dxf_code.append("%.4f" % (0))       #z1 coord
-            dxf_code.append(" 11")
-            dxf_code.append("%.4f" % (line[2])) #x2 coord
-            dxf_code.append(" 21")
-            dxf_code.append("%.4f" % (line[3])) #y2 coord
-            dxf_code.append(" 31")
-            dxf_code.append("%.4f" % (0))       #z2 coord
-            dxf_code.append("  0")
-
-        dxf_code.append("ENDSEC")
-        dxf_code.append("0")
-        dxf_code.append("EOF")
-        ######################################
-        ## END G-CODE WRITING for Dxf_Write ##
-        ######################################
-        return dxf_code
-
-
     def CopyClipboard_GCode(self):
         self.clipboard_clear()
         if (self.Check_All_Variables() > 0):
@@ -3911,8 +3742,10 @@ class Application(Frame):
         self.menu_File_Save_DXF_File(close_loops=True)
 
     def menu_File_Save_DXF_File(self, close_loops=False):
+        if close_loops:
+            self.V_Carve_It(clean_flag=0, DXF_FLAG = close_loops)
 
-        DXF_CODE = self.WriteDXF(close_loops=close_loops)
+        DXF_CODE = WriteDXF(self.coords)
         init_dir = os.path.dirname(self.NGC_FILE)
         if ( not os.path.isdir(init_dir) ):
             init_dir = self.HOME_DIR
